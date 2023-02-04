51d8b6f077a7bb8a88e7d1d5a7acd9698b71c00e
anhalu <anhalu.dev@gmail.com>
2023-02-04 14:50:07 +0700

    emotion detection

---
+++ emotion_detection.py
@@ -0,0 +1,57 @@
+import cv2
+import mediapipe as mp
+import tensorflow as tf
+
+mp_face_detection = mp.solutions.face_detection
+mp_drawing = mp.solutions.drawing_utils
+
+model = tf.keras.models.load_model('model1.h5')
+
+emotion_names = ['angry', 'disgusted', 'fearful', 'happy', 'neutral', 'sad', 'surprised']
+
+
+cap = cv2.VideoCapture(0)
+with mp_face_detection.FaceDetection(
+    model_selection=0, min_detection_confidence=0.5) as face_detection:
+  while cap.isOpened():
+    success, image = cap.read()
+    if not success:
+      print("Ignoring empty camera frame.")
+      continue
+
+    image.flags.writeable = False
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    results = face_detection.process(image)
+
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    if results.detections:
+      for detection in results.detections:
+        box = detection.location_data.relative_bounding_box
+        x_start, y_start = int(box.xmin * image.shape[1]) - 5 , int(box.ymin * image.shape[0]) - 50
+        x_end, y_end = int((box.xmin + box.width) * image.shape[1])+5 , int((box.ymin + box.height)* image.shape[0])
+        img_new = image[y_start:y_end, x_start:x_end]
+        
+        try : 
+          img_new = cv2.resize(img_new, (48, 48))
+          img_new = img_new.reshape((1,) + img_new.shape)
+          img_new = img_new/255.0 
+          predictions = model.predict(img_new) 
+          id = predictions.argmax(axis=1)[0]
+          print(emotion_names[id])
+          cv2.putText(image, emotion_names[id],(x_start, y_start - 10), color=(255,0,0), fontScale=2, thickness=2, fontFace=cv2.FONT_HERSHEY_DUPLEX)
+          cv2.rectangle(image, (x_start, y_start), (x_end, y_end), (0,0,255), 5)
+          
+        except Exception as e : 
+          print(e) 
+        
+        # score = round(detection.score[0]*100)
+        # cv2.putText(image, str(score) + "%",(x_start, y_start), color=(255,0,0), fontScale=2, thickness=2, fontFace=cv2.FONT_HERSHEY_DUPLEX)
+        # cv2.rectangle(image, (x_start, y_start), (x_end, y_end), (0,0,255), 5)
+        # mp_drawing.draw_detection(image, detection)
+    # cv2.imshow('Emotion Detection', image)
+    cv2.imshow('Emotion Detection', cv2.flip(image, 1))
+    
+    if cv2.waitKey(5) & 0xFF == ord('q'):
+      break
+cap.release()
